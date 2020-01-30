9ef429eb15715f62db7f6022c6e6b43e344f43df
Lucas <096128@hr.nl>
2020-01-30 12:25:32 +0100

    components en routing
    meerdere components geïmporteerd en de routes naar de components goedgezet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import './App.css'
 import Library from './components/library'
+import EditGame from './components/editgame'
+import CreateGame from './components/creategame'
+import { render } from '@testing-library/react'
 
 function App() {
   return (
-    <div className="Library">
-      <Library/>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" exact component={Library}/>
+        <Route path="/edit/:id" component={EditGame}/>
+        <Route path="/create" component={CreateGame}/>
+      </div>
+    </Router>
   );
 }
 
