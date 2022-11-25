493c382c623e053ce87dc607c0272373bb3afb63
Lucas Hijman <lucashijman12@gmail.com>
2022-11-25 16:04:04 +0100

    update form views with reactstrap library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap'
+import { Button, Col, Form, FormGroup, Input, Label, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap'
 import axios from 'axios'
+import FormTitle from './components/FormTitle'
 
 class App extends Component {
   constructor(props) {
@@ -31,27 +32,31 @@ class App extends Component {
 
   // Game aanmaken
   onChangeName(e) {
-      this.setState({
-          name: e.target.value
-      })
+    console.log("Verander naam")
+    this.setState({
+      name: e.target.value
+    })
   }
 
   onChangeCompany(e) {
-      this.setState({
-          company: e.target.value
-      })
+    console.log("Verander bedrijf")
+    this.setState({
+      company: e.target.value
+    })
   }
 
   onChangeConsole(e) {
-      this.setState({
-          console: e.target.value
-      })
+    console.log("Verander console")
+    this.setState({
+      console: e.target.value
+    })
   }
 
   onChangeRelease(e) {
-      this.setState({
-          release: e.target.value
-      })
+    console.log("Verander releasedatum")
+    this.setState({
+      release: e.target.value
+    })
   }
 
   onSubmit(e) {
@@ -64,6 +69,8 @@ class App extends Component {
         release: this.state.release
     }
 
+    console.log("Nieuwe game aanmaken")
+
     axios.post(`http://145.24.222.100:8001/games`, newGame).then((response) => {
       this.setState({
         name: '',
@@ -73,10 +80,14 @@ class App extends Component {
       })
       this.autoReloadGames()
     })
+
+    this.toggleCreateGameModal()
   }
   
   // Collectie laden
   componentWillMount() {
+    console.log("ComponentWillMount")
+
     axios.get('http://145.24.222.100:8001/games', {
       headers: {
         'Accept':'application/json'
@@ -90,39 +101,50 @@ class App extends Component {
 
   // Item verwijderen
   deleteGame(_id) {
+    console.log("Verwijder game")
+
     axios.delete('http://145.24.222.100:8001/games/' + _id).then((response) => {
       this.autoReloadGames()
     })
   }
 
-  // Item updaten
+  // Te bewerken item inladen
   editGame(_id, name, company, console, release) {
+    console.log("Laad game informatie in")
+
     this.setState({
       editGameData: { _id, name, company, console, release }, editGameModal: ! this.state.editGameModal
     })
   }
 
+  // Item updaten
   updateGame() {
+    console.log("Bewerk game")
+
     let { name, company, console, release } = this.state.editGameData
+
     axios.put('http://145.24.222.100:8001/games/' + this.state.editGameData._id, {
       name, company, console, release 
     }, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
-      }
-    }).then((response) => {
+    }}).then((response) => {
       this.autoReloadGames()
     })
   }
 
   toggleCreateGameModal() {
+    console.log("Toggle create game modal")
+
     this.setState({
       createGameModal: !this.state.createGameModal
     })
   }
 
   toggleEditGameModal() {
+    console.log("Toggle edit game modal")
+
     this.setState({
       editGameModal: !this.state.editGameModal
     })
@@ -130,6 +152,8 @@ class App extends Component {
 
   // Interface gelijk updaten na CRUD actie
   autoReloadGames() {
+    console.log("Automatisch games herladen")
+
     axios.get('http://145.24.222.100:8001/games', {
       headers: {
         'Accept':'application/json'
@@ -144,12 +168,12 @@ class App extends Component {
   // Interface rendering
   render() {
     let games = this.state.games.map((game) => {
-      return(
+      return (
         <tr key={game._id}>
           <td>
           <p><a class="popup" href="/">{game.name}<span class="classic">Company: {game.company}<br></br>Console: {game.console}<br></br>Release: {game.release}</span></a></p></td>
           <td>
-            <Button color="success" size="sm" className="mr-2" onClick={this.editGame.bind(this, game._id, game.name, game.company, game.console, game.release)}>Edit</Button>
+            <Button color="warning" size="sm" className="mr-2" onClick={this.editGame.bind(this, game._id, game.name, game.company, game.console, game.release)}>Edit</Button>
             <Button color="danger" size="sm" onClick={this.deleteGame.bind(this, game._id)}>Delete</Button>
           </td>
         </tr>
@@ -158,78 +182,100 @@ class App extends Component {
 
     return (
       <div>
-        {/*Return de library*/}
-        {games}<br></br><br></br>
-        <Button color="primary" onClick={this.toggleCreateGameModal.bind(this)}>
-          Nieuw spel
-        </Button>
+        <Button className="mt-3 ml-3" color="primary" onClick={this.toggleCreateGameModal.bind(this)}>Nieuw spel</Button>
 
-        {/*Return de aanmaakform*/}
+        {/* Display game library */}
+        {games}
+
+        {/* Create game modal */}
         <Modal isOpen={this.state.createGameModal} toggle={this.toggle}>
-          <ModalHeader/>
+          <ModalHeader>
+            <FormTitle value={"Nieuw spel"} />
+          </ModalHeader>
           <ModalBody>
-            <form onSubmit={this.onSubmit}>
-              <h3>Voeg een game toe</h3>
-              <div>
-                <label>Name: </label>
-                <input type="text" onChange={this.onChangeName}></input>
-              </div><div>
-                <label>Company: </label>
-                <input type="text" onChange={this.onChangeCompany}></input>
-              </div><div>
-                <label>Console: </label>
-                <input type="text" onChange={this.onChangeConsole}></input>
-              </div><div>
-                <label>Release: </label>
-                <input type="text" onChange={this.onChangeRelease}></input>
-              </div>
-            </form>
+            <Form formTitle="Nieuw spel" onSubmit={this.onSubmit}>
+              <FormGroup row>
+                <Label for="game_name" sm={3}>Name</Label>
+                <Col sm={9}>
+                  <Input type="text" name="game_name" id="game_name" onChange={this.onChangeName}/>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="game_company" sm={3}>Campany</Label>
+                <Col sm={9}>
+                  <Input type="text" name="game_company" id="game_company" onChange={this.onChangeCompany}/>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="game_console" sm={3}>Console</Label>
+                <Col sm={9}>
+                  <Input type="text" name="game_console" id="game_console" onChange={this.onChangeConsole}/>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="game_release" sm={3}>Release date</Label>
+                <Col sm={9}>
+                  <Input type="date" name="game_release" id="game_release" onChange={this.onChangeRelease}/>
+                </Col>
+              </FormGroup>
+            </Form>
           </ModalBody>
           <ModalFooter>
-            <Button onClick={this.onSubmit.bind(this)}>Update</Button>
-            <Button onClick={this.toggleCreateGameModal.bind(this)}>Annuleer</Button>
+            <Button color="success" onClick={this.onSubmit.bind(this)}>Aanmaken</Button>
+            <Button color="danger" onClick={this.toggleCreateGameModal.bind(this)}>Annuleren</Button>
           </ModalFooter>
         </Modal>
 
-        {/* Return de updateform*/}
+        {/* Edit game modal */}
         <Modal isOpen={this.state.editGameModal} toggle={this.toggleEditGameModal.bind(this)}>
           <ModalHeader toggle={this.toggleEditGameModal.bind(this)}></ModalHeader>
           <ModalBody>
-          <form onSubmit={this.onSubmit}>
-            <div>
-              <label>Name: </label>
-              <input type="text" value={this.state.editGameData.name} onChange={(e) => {
-                let { editGameData } = this.state
-                editGameData.name = e.target.value
-                this.setState({ editGameData })
-              }}></input>
-            </div><div>
-              <label>Company: </label>
-              <input type="text" value={this.state.editGameData.company} onChange={(e) => {
-                let { editGameData } = this.state
-                editGameData.company = e.target.value
-                this.setState({ editGameData })
-              }}></input>
-            </div><div>
-              <label>Console: </label>
-              <input type="text" value={this.state.editGameData.console} onChange={(e) => {
-                let { editGameData } = this.state
-                editGameData.console = e.target.value
-                this.setState({ editGameData })
-              }}></input>
-            </div><div>
-              <label>Release: </label>
-              <input type="text" value={this.state.editGameData.release} onChange={(e) => {
-                let { editGameData } = this.state
-                editGameData.release = e.target.value
-                this.setState({ editGameData })
-              }}></input>
-            </div>
-          </form>
+            <Form>
+              <FormGroup>
+                <Label for="game_name" sm={3}></Label>
+                <Col cm={9}>
+                  <Input type="text" name="edit_game_name" id="game_name" value={this.state.editGameData.name} onChange={(e) => {
+                  let { editGameData } = this.state
+                  editGameData.name = e.target.value
+                  this.setState({ editGameData })
+                }}/>
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Label for="game_company" sm={3}></Label>
+                <Col cm={9}>
+                  <Input type="text" name="game_company" id="game_company" value={this.state.editGameData.company} onChange={(e) => {
+                  let { editGameData } = this.state
+                  editGameData.company = e.target.value
+                  this.setState({ editGameData })
+                }}/>
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Label for="game_console" sm={3}></Label>
+                <Col cm={9}>
+                  <Input type="text" name="game_console" id="game_console" value={this.state.editGameData.console} onChange={(e) => {
+                  let { editGameData } = this.state
+                  editGameData.console = e.target.value
+                  this.setState({ editGameData })
+                }}/>
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Label for="game_release" sm={3}></Label>
+                <Col cm={9}>
+                  <Input type="date" name="game_release" id="game_release" value={this.state.editGameData.release} onChange={(e) => {
+                  let { editGameData } = this.state
+                  editGameData.release = e.target.value
+                  this.setState({ editGameData })
+                }}/>
+                </Col>
+              </FormGroup>
+            </Form>
           </ModalBody>
           <ModalFooter>
-            <Button onClick={this.updateGame.bind(this)}>Update</Button>
-            <Button onClick={this.toggleEditGameModal.bind(this)}>Annuleer</Button>
+            <Button onClick={this.updateGame.bind(this)}>Bijwerken</Button>
+            <Button onClick={this.toggleEditGameModal.bind(this)}>Annuleren</Button>
           </ModalFooter>
         </Modal>
       </div>
